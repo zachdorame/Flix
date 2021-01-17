a9e4dbb91e417705a060e3e9f0e675df8d69fcf9
zach dorame barajas <zachdoramebarajas@gmail.com>
2021-01-16 17:52:35 -0800

    User can see detail screen for superhero movies

--- flix/MovieGridViewController.swift
+++ flix/MovieGridViewController.swift
@@ -55,6 +55,7 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieGridCell", for: indexPath) as! MovieGridCell
         
         let movie = movies[indexPath.item]
@@ -69,14 +70,23 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
     }
     
 
-    /*
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        let cell = sender as! UICollectionViewCell
+        let indexPath = collectionView.indexPath(for: cell)
+        let movie = movies[indexPath?.row ?? 0]
+        
+        let detailsViewController = segue.destination as! SuperheroViewController
+        detailsViewController.movie = movie
+        
+        // still need to deselect? not sure
     }
-    */
+
 
 }
---
+++ flix/SuperheroViewController.swift
@@ -0,0 +1,52 @@
+//
+//  SuperheroViewController.swift
+//  flix
+//
+//  Created by Zach Dorame-Barajas on 1/16/21.
+//
+
+import UIKit
+
+class SuperheroViewController: UIViewController {
+
+    @IBOutlet weak var superheroPoster: UIImageView!
+    @IBOutlet weak var superheroPreview: UIImageView!
+    @IBOutlet weak var superheroSynopsis: UILabel!
+    @IBOutlet weak var superheroTitle: UILabel!
+    
+    var movie: [String: Any]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        superheroTitle.text = movie["title"] as? String
+        superheroTitle.sizeToFit()
+        
+        superheroSynopsis.text = movie["overview"] as? String
+        superheroSynopsis.sizeToFit()
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        superheroPreview.af.setImage(withURL: posterUrl!)
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + posterPath)
+        superheroPoster.af.setImage(withURL: backdropUrl!)
+    }
+    
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
+}
