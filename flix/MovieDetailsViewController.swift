f0e7e40a9176296066317c60e8b7e83733d05d18
zach dorame barajas <zachdoramebarajas@gmail.com>
2021-01-17 18:01:02 -0800

    Posters show movie preview

--- flix/MovieDetailsViewController.swift
+++ flix/MovieDetailsViewController.swift
@@ -16,8 +16,10 @@ class MovieDetailsViewController: UIViewController {
     @IBOutlet weak var synopsisLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var previewButton: UIButton!
     
     var movie: [String:Any]!
+    var trailers = [[String:Any]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,14 +43,50 @@ class MovieDetailsViewController: UIViewController {
     }
     
 
-    /*
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        /*
+        let movieId = (movie["id"] as? Int)!
+        let movieIdStr = String(movieId)
+        let apiPathOne = "https://api.themoviedb.org/3/movie/"
+        let apiPathTwo = "/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        
+        // print(apiPathOne + movieIdStr + apiPathTwo)
+        // var trailers: [[String:Any]]
+        let url = URL(string: apiPathOne + movieIdStr + apiPathTwo)!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+           // This will run when the network request returns
+           if let error = error {
+              print(error.localizedDescription)
+           } else if let data = data {
+            let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+            
+            self.trailers = dataDictionary["results"] as! [[String:Any]]
+            //print(self.trailers)
+            //print("DATADICT")
+            //print(dataDictionary)
+            //print("end DATA DICT")
+            // self.trailers.reloadData()
+           }
+        }
+        task.resume()
+        
+        print(trailers)
+        */
+        
+        let detailsViewController = segue.destination as! MoviePreviewViewController
+        detailsViewController.movie = movie
+        
+        
     }
-    */
+
 
 }
---
+++ flix/MoviePreviewViewController.swift
@@ -0,0 +1,69 @@
+//
+//  MoviePreviewViewController.swift
+//  flix
+//
+//  Created by Zach Dorame-Barajas on 1/16/21.
+//
+
+import UIKit
+import WebKit
+
+class MoviePreviewViewController: UIViewController {
+
+    @IBOutlet weak var moviePreview: WKWebView!
+    
+    var movie: [String:Any]!
+    var trailers = [[String:Any]]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let movieId = (movie["id"] as? Int)!
+        let movieIdStr = String(movieId)
+        let apiPathOne = "https://api.themoviedb.org/3/movie/"
+        let apiPathTwo = "/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        
+        // print(apiPathOne + movieIdStr + apiPathTwo)
+        // var trailers: [[String:Any]]
+        let url = URL(string: apiPathOne + movieIdStr + apiPathTwo)!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+           // This will run when the network request returns
+           if let error = error {
+              print(error.localizedDescription)
+           } else if let data = data {
+            let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+            
+            self.trailers = dataDictionary["results"] as! [[String:Any]]
+            
+            let key = self.trailers[0]["key"] as! String
+            let myURL = URL(string: "https://www.youtube.com/watch?v=" + key)
+            let myRequest = URLRequest(url: myURL!)
+            self.moviePreview.load(myRequest)
+            
+            
+           }
+        }
+        task.resume()
+        
+        // print("printing trailers")
+        //print(trailers)
+        // print("done printing trailers")
+        //let myURL = URL(string: "https://www.youtube.com/watch?v=" + (trailers["key"] as! String))
+        //let myRequest = URLRequest(url: myURL!)
+        //moviePreview.load(myRequest)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
